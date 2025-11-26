10fa355971bed94a58d070b9982965f68c70e529
Pedrinho Hilário <flygames1214@gmail.com>
2025-11-26 15:18:45 -0300

    colocando iframe do youtube na home do site abaixo do banner

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,9 @@ export default function HomePage() {
   return (
     <main className="pt-20">
       <Banner />
+      <section className="flex my-10 justify-center items-center">
+        <iframe width="860" height="615" src="https://www.youtube.com/embed/0EZpoKan4dY?si=gT7F-5YVUNs19bpN" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="rounded-2xl"></iframe>
+      </section>
       <BlocoDois />
       <BlocoTres />
     </main>
